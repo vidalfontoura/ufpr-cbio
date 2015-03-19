d299a3082221936a3dcd4df465f619003c13eb35
Vidal Fontoura <vfontoura@ziji-media.com>
2015-03-19 15:39:32 -0300

    Commit to backup code

--- src/main/java/org/ufpr/cbio/poc/utils/ResidueUtils.java
+++ src/main/java/org/ufpr/cbio/poc/utils/ResidueUtils.java
@@ -93,6 +93,232 @@ public class ResidueUtils {
         return residues;
     }
 
+    public static List<Residue> applySolution(List<Residue> residues, int[] solution) {
+
+        // Definition of solution
+        // 1 - Clockwise
+        // 2 - Two Clockwise
+        // 3 - Corner
+
+        // This will only work with the residue list it is order of insertion
+        List<Residue> newResidues = new ArrayList<Residue>(residues);
+        Residue previous = null;
+        Residue next = null;
+        for (int i = 0; i < residues.size(); i++) {
+            Residue residue = residues.get(i);
+
+            int movement = solution[i];
+            if (i > 0) {
+                previous = residues.get(i - 1);
+            }
+            if (i + 1 < residues.size()) {
+                next = residues.get(i + 1);
+            }
+            switch (movement) {
+                case 1:
+                    // TODO:
+                    break;
+                case 2:
+                    // TODO:
+                    break;
+                case 3:
+                    // check if it is a corner
+                    if (i - 1 >= 0 && i + 1 > 1) {
+                        if (previous != null && next != null) {
+                            // Check if is a upper left corner
+                            if (isUpperLeftCorner(residue, previous, next)) {
+                                // Check if it is possible to make the move
+                                int xnew = residue.getPoint().getX() + 1;
+                                int ynew = residue.getPoint().getY() - 1;
+                                if (!isPointSet(residues, xnew, ynew)) {
+                                    // Make the move
+                                    newResidues.get(i).setPoint(new Point(xnew, ynew));
+                                    break;
+                                }
+                            }
+                            // Check if is a bottom left corner
+                            if (isBottomLeftCorner(residue, previous, next)) {
+                                // Check if it is possible to make the move
+                                int xnew = residue.getPoint().getX() + 1;
+                                int ynew = residue.getPoint().getY() + 1;
+                                if (!isPointSet(residues, xnew, ynew)) {
+                                    // Make the move
+                                    newResidues.get(i).setPoint(new Point(xnew, ynew));
+                                    break;
+                                }
+                            }
+                            // Check if is bottom right corner
+                            if (isBottomRightCorner(residue, previous, next)) {
+                                // Check if it is possible to make the move
+                                int xnew = residue.getPoint().getX() - 1;
+                                int ynew = residue.getPoint().getY() + 1;
+                                if (!isPointSet(residues, xnew, ynew)) {
+                                    // Make the move
+                                    newResidues.get(i).setPoint(new Point(xnew, ynew));
+                                    break;
+                                }
+                            }
+                            // Check if is upper right corner
+                            if (isUpperRightCorner(residue, previous, next)) {
+                                // Check if it is possible to make the move
+                                int xnew = residue.getPoint().getX() - 1;
+                                int ynew = residue.getPoint().getY() - 1;
+                                if (!isPointSet(residues, xnew, ynew)) {
+                                    // Make the move
+                                    newResidues.get(i).setPoint(new Point(xnew, ynew));
+                                    break;
+                                }
+                            }
+                        }
+                        // If does not have next == null || previous it is
+                        // impossible to be a corner
+                    }
+                    break;
+                case 4: {
+                    if (previous != null && next != null) {
+                        if (isUpperLeftCorner(residue, previous, next)) {
+                            int xnew = residue.getPoint().getX() + 1;
+                            int ynew = residue.getPoint().getY() - 1;
+                            if (isPointSet(residues, xnew, ynew)) {
+                                int xnewCrankshaft = residue.getPoint().getX() + 2;
+                                if (!isPointSet(residues, xnewCrankshaft, residue.getPoint().getY())
+                                    && !isPointSet(residues, xnewCrankshaft, residue.getPoint().getY() - 1)) {
+                                    // Make crankshaft move
+                                    newResidues.get(i).setPoint(new Point(xnewCrankshaft, residue.getPoint().getY()));
+                                    newResidues.get(i + 1).setPoint(
+                                        new Point(xnewCrankshaft, residue.getPoint().getY() - 1));
+                                }
+                            }
+                        }
+                        if (isBottomLeftCorner(residue, previous, next)) {
+                            int xnew = residue.getPoint().getX() + 1;
+                            int ynew = residue.getPoint().getY() + 1;
+                            if (isPointSet(residues, xnew, ynew)) {
+                                int xnewCrankshaft = residue.getPoint().getX() + 2;
+                                if (!isPointSet(residues, xnewCrankshaft, residue.getPoint().getY())
+                                    && !isPointSet(residues, xnewCrankshaft, residue.getPoint().getY() + 1)) {
+                                    // Make crankshaft move
+                                    newResidues.get(i).setPoint(new Point(xnewCrankshaft, residue.getPoint().getY()));
+                                    newResidues.get(i + 1).setPoint(
+                                        new Point(xnewCrankshaft, residue.getPoint().getY() + 1));
+                                }
+                            }
+                        }
+
+                        if (isUpperRightCorner(residue, previous, next)) {
+                            int xnew = residue.getPoint().getX() - 1;
+                            int ynew = residue.getPoint().getY() - 1;
+                            if (isPointSet(residues, xnew, ynew)) {
+                                int xnewCrankshaft = residue.getPoint().getX() - 2;
+                                if (!isPointSet(residues, xnewCrankshaft, residue.getPoint().getY())
+                                    && !isPointSet(residues, xnewCrankshaft, residue.getPoint().getY() - 1)) {
+                                    // Make crankshaft move
+                                    newResidues.get(i).setPoint(new Point(xnewCrankshaft, residue.getPoint().getY()));
+                                    newResidues.get(i + 1).setPoint(
+                                        new Point(xnewCrankshaft, residue.getPoint().getY() - 1));
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+        return newResidues;
+    }
+
+    public static boolean isUpperLeftCorner(Residue residue, Residue previous, Residue next) {
+
+        if (previous.getPoint().getX() == residue.getPoint().getX()
+            && previous.getPoint().getY() + 1 == residue.getPoint().getY()
+            && next.getPoint().getX() - 1 == residue.getPoint().getX()
+            && next.getPoint().getY() == residue.getPoint().getY()
+            || (next.getPoint().getX() == residue.getPoint().getX()
+                && next.getPoint().getY() + 1 == residue.getPoint().getY()
+                && previous.getPoint().getX() - 1 == residue.getPoint().getX() && previous.getPoint().getY() == residue
+                .getPoint().getY())) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isBottomLeftCorner(Residue residue, Residue previous, Residue next) {
+
+        if (previous.getPoint().getX() - 1 == residue.getPoint().getX()
+            && previous.getPoint().getY() == residue.getPoint().getY()
+            && next.getPoint().getX() == residue.getPoint().getX()
+            && next.getPoint().getY() - 1 == residue.getPoint().getY()
+            || (next.getPoint().getX() - 1 == residue.getPoint().getX()
+                && next.getPoint().getY() == residue.getPoint().getY()
+                && previous.getPoint().getX() == residue.getPoint().getX() && previous.getPoint().getY() - 1 == residue
+                .getPoint().getY())) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isBottomRightCorner(Residue residue, Residue previous, Residue next) {
+
+        if (previous.getPoint().getX() == residue.getPoint().getX()
+            && previous.getPoint().getY() - 1 == residue.getPoint().getY()
+            && next.getPoint().getX() + 1 == residue.getPoint().getX()
+            && next.getPoint().getY() == residue.getPoint().getY()
+            || (next.getPoint().getX() == residue.getPoint().getX()
+                && next.getPoint().getY() - 1 == residue.getPoint().getY()
+                && previous.getPoint().getX() + 1 == residue.getPoint().getX() && previous.getPoint().getY() == residue
+                .getPoint().getY())) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public static boolean isUpperRightCorner(Residue residue, Residue previous, Residue next) {
+
+        if (previous.getPoint().getX() + 1 == residue.getPoint().getX()
+            && previous.getPoint().getY() == residue.getPoint().getY()
+            && next.getPoint().getX() == residue.getPoint().getX()
+            && next.getPoint().getY() + 1 == residue.getPoint().getY()
+            || (next.getPoint().getX() + 1 == residue.getPoint().getX()
+                && next.getPoint().getY() == residue.getPoint().getY()
+                && previous.getPoint().getX() == residue.getPoint().getX() && previous.getPoint().getY() + 1 == residue
+                .getPoint().getY())) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public static boolean isBottomLeftCornorCrankshaft(Residue residue, Residue previous, Residue next) {
+        //TODO:FIX IT
+       if( isBottomLeftCorner(residue, previous, next)) {
+            int xnew = residue.getPoint().getX() + 1;
+            int ynew = residue.getPoint().getY() + 1;
+            if (isPointSet(residues, xnew, ynew)) {
+                int xnewCrankshaft = residue.getPoint().getX() + 2;
+                if (!isPointSet(residues, xnewCrankshaft, residue.getPoint().getY())
+                    && !isPointSet(residues, xnewCrankshaft, residue.getPoint().getY() + 1)) {
+                    // Make crankshaft move
+                    newResidues.get(i).setPoint(new Point(xnewCrankshaft, residue.getPoint().getY()));
+                    newResidues.get(i + 1).setPoint(
+                        new Point(xnewCrankshaft, residue.getPoint().getY() + 1));
+                }
+            }
+    }
+
+    public static boolean isPointSet(List<Residue> residues, int x, int y) {
+
+        for (Residue residue : residues) {
+            if (residue.getPoint().getX() == x && residue.getPoint().getY() == y) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
     public static Residue.Point createInitialPoint(int max) {
 
         Random random = new Random();
--- src/main/java/org/ufpr/cbio/poc/utils/TestRepair.java
+++ src/main/java/org/ufpr/cbio/poc/utils/TestRepair.java
@@ -3,6 +3,13 @@
  */
 package org.ufpr.cbio.poc.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ufpr.cbio.poc.domain.Residue;
+import org.ufpr.cbio.poc.domain.Residue.Point;
+import org.ufpr.cbio.poc.domain.ResidueType;
+
 /**
  *
  *
@@ -12,17 +19,91 @@ public class TestRepair {
 
     public static void main(String[] args) {
 
-        int[] x = new int[] { 1, 0, -1, -2, -3, -4, -4, -5, -5, -5, -5 };
-        int[] y = new int[] { 2, 2, 2, 2, 2, 2, 3, 3, 2, 1, 0 };
-        for (int i = 0; i < x.length; i++) {
-            x[i] = x[i] - (-5);
-            System.out.print(x[i]);
-        }
-        System.out.println();
+        // int[] x = new int[] { 1, 0, -1, -2, -3, -4, -4, -5, -5, -5, -5 };
+        // int[] y = new int[] { 2, 2, 2, 2, 2, 2, 3, 3, 2, 1, 0 };
+        // for (int i = 0; i < x.length; i++) {
+        // x[i] = x[i] - (-5);
+        // System.out.print(x[i]);
+        // }
+        // System.out.println();
+        //
+        // for (int j = 0; j < y.length; j++) {
+        // y[j] = y[j] - (0);
+        // System.out.print(y[j]);
+        // }
 
-        for (int j = 0; j < y.length; j++) {
-            y[j] = y[j] - (0);
-            System.out.print(y[j]);
+        List<Residue> residues = new ArrayList<Residue>();
+        // residues.add(new Residue(new Point(1, 1), ResidueType.H));
+        // residues.add(new Residue(new Point(1, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(3, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(4, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(5, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(5, 1), ResidueType.H));
+
+        // upper left corner
+        // residues.add(new Residue(new Point(2, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(1, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(1, 3), ResidueType.H));
+
+        // bottom left corner
+        // residues.add(new Residue(new Point(3, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(4, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(4, 4), ResidueType.H));
+
+        // residues.add(new Residue(new Point(1, 1), ResidueType.H));
+        // residues.add(new Residue(new Point(1, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(1, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 5), ResidueType.H));
+        // residues.add(new Residue(new Point(3, 5), ResidueType.H));
+        // residues.add(new Residue(new Point(4, 5), ResidueType.H));
+        // residues.add(new Residue(new Point(4, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(4, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(5, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(5, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(5, 1), ResidueType.H));
+        // residues.add(new Residue(new Point(4, 1), ResidueType.H));
+        // residues.add(new Residue(new Point(3, 1), ResidueType.H));
+        // residues.add(new Residue(new Point(3, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 2), ResidueType.H));
+
+        // int[] x = new int[] { 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+        // 3 };
+
+        // int[] x = new int[] { 1, 3, 3 };
+
+        // First crankshaft
+        // residues.add(new Residue(new Point(3, 5), ResidueType.H));
+        // residues.add(new Residue(new Point(3, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(3, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(3, 2), ResidueType.H));
+
+        // Second crankshaft
+        // residues.add(new Residue(new Point(2, 1), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 2), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(1, 3), ResidueType.H));
+        // residues.add(new Residue(new Point(1, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 4), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 5), ResidueType.H));
+        // residues.add(new Residue(new Point(2, 6), ResidueType.H));
+        //
+
+        residues.add(new Residue(new Point(1, 4), ResidueType.H));
+        residues.add(new Residue(new Point(1, 3), ResidueType.H));
+        residues.add(new Residue(new Point(2, 3), ResidueType.H));
+        residues.add(new Residue(new Point(2, 2), ResidueType.H));
+        residues.add(new Residue(new Point(1, 2), ResidueType.H));
+        residues.add(new Residue(new Point(1, 1), ResidueType.H));
+        int[] x = new int[] { 4, 4, 4, 4, 4, 4 };
+        List<Residue> applySolution = ResidueUtils.applySolution(residues, x);
+
+        for (Residue residue : applySolution) {
+            System.out.println(residue.getPoint().getX() + "," + residue.getPoint().getY());
         }
 
     }
