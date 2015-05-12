affc6e730bd868570ec671d23512dc2ba0ac4887
Ricardo Lima <ricardo_des@hotmail.com>
2015-05-12 14:54:49 -0300

    + Upgrade in the Graphical Visualization. - Still can't rezise the scene according to the protein chain.

--- src/main/java/org/ufpr/cbio/poc/visual/ApplyFixedRelativeSolution.java
+++ src/main/java/org/ufpr/cbio/poc/visual/ApplyFixedRelativeSolution.java
@@ -3,13 +3,17 @@ package org.ufpr.cbio.poc.visual;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,7 +21,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JViewport;
+import javax.swing.border.Border;
 
+import org.jfree.chart.block.LineBorder;
 import org.ufpr.cbio.poc.domain.Grid;
 import org.ufpr.cbio.poc.domain.Residue;
 import org.ufpr.cbio.poc.domain.Residue.Point;
@@ -32,10 +38,10 @@ import org.ufpr.cbio.poc.utils.ResidueUtils;
  */
 public class ApplyFixedRelativeSolution {
 
-    // private static final String PROTEIN_CHAIN = "HPHPPHHPHPPHPHHPPHPH";
+//     private static final String PROTEIN_CHAIN = "HPHPPHHPHPPHPHHPPHPH";
 
-    // private static final String PROTEIN_CHAIN =
-    // "HHHHPPPPHHHHHHHHHHHHPPPPPPHHHHHHHHHHHHPPPHHHHHHHHHHHHPPPHHHHHHHHHHHHPPPHPPHHPPHHPPHPH";
+//     private static final String PROTEIN_CHAIN = 
+//    		 "HHHHPPPPHHHHHHHHHHHHPPPPPPHHHHHHHHHHHHPPPHHHHHHHHHHHHPPPHHHHHHHHHHHHPPPHPPHHPPHHPPHPH";
 
     private static final String PROTEIN_CHAIN =
         "PPPPPPHPHHPPPPPHHHPHHHHHPHHPPPPHHPPHHPHHHHHPHHHHHHHHHHPHHPHHHHHHHPPPPPPPPPPPHHHHHHHPPHPHHHPPPPPPHPHH";
@@ -44,8 +50,8 @@ public class ApplyFixedRelativeSolution {
     private static final int MIN_SIZE_FACTOR = 20;
     private static final int MAX_SIZE_SCROLL_PANEL = 500;
 
-    private static int sizeFactor = 40;
-    private static int maxGridSize = 600;
+    private static int sizeFactor = 20;
+    private static int maxGridSize = 500;
     private static int slots = 0;
     private static List<Residue> residues;
     private static int energyValue = 0;
@@ -56,25 +62,19 @@ public class ApplyFixedRelativeSolution {
 
     public static void addComponentsToPane(Container pane) {
 
-        pane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
+        pane.setLayout(new FlowLayout());
 
         MyCanvas myCanvas = new MyCanvas();
-        if (grid == null) {
-            slots = 0;
-            maxGridSize = 0;
-            sizeFactor = 0;
-        }
-
-        myCanvas.setPreferredSize(new Dimension(maxGridSize, maxGridSize));
-
-        JScrollPane jScrollPane = new JScrollPane(myCanvas);
+        
+        JScrollPane jScrollPane = new JScrollPane(myCanvas, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        jScrollPane.setPreferredSize(new Dimension(500, 500));
         jScrollPane.setBounds(5, 30, MAX_SIZE_SCROLL_PANEL, MAX_SIZE_SCROLL_PANEL);
         jScrollPane.setWheelScrollingEnabled(true);
         jScrollPane.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
-
-        JTextField solutionTextField = new JTextField("Enter the solution");
+       
+        JTextField chainTextField = new JTextField(PROTEIN_CHAIN);
         JButton applyMovementsButton = new JButton("Apply Movements");
+        JTextField solutionTextField = new JTextField("Enter the solution");
         JButton printPointsButton = new JButton("Print Points");
 
         JButton resetButton = new JButton("Reset");
@@ -84,75 +84,35 @@ public class ApplyFixedRelativeSolution {
         JLabel maxDistanceLabel = new JLabel("Max Distance: " + maxDistance);
         JLabel fitnessLabel = new JLabel("Fitness: " + (energyValue - collisions));
 
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        pane.add(applyMovementsButton, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 1;
-        c.gridy = 0;
-        pane.add(solutionTextField, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 2;
-        c.gridy = 0;
-        pane.add(resetButton, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 3;
-        c.gridy = 0;
-        pane.add(printPointsButton, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 0;
-        c.gridy = 1;
-        pane.add(energyLabel, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 2;
-        c.gridy = 1;
-        pane.add(collisionsLabel, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 3;
-        c.gridy = 1;
-        pane.add(chainLengthLabel, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 4;
-        c.gridy = 1;
-        pane.add(maxDistanceLabel, c);
-
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        c.gridx = 5;
-        c.gridy = 1;
-        pane.add(fitnessLabel, c);
-
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
-        c.weighty = 1;
-        c.gridwidth = 4;
-        c.gridx = 0;
-        c.gridy = 2;
-
-        pane.add(jScrollPane, c);
-
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        
+        panel2.setPreferredSize(new Dimension(200, 400));
+        panel2.setLayout(new GridLayout(10, 1));
+        
+        panel1.add(jScrollPane);
+        
+        panel2.add(chainTextField);
+        panel2.add(applyMovementsButton);
+        panel2.add(solutionTextField);
+        panel2.add(chainLengthLabel);
+        panel2.add(energyLabel);
+        panel2.add(maxDistanceLabel);
+        panel2.add(fitnessLabel);
+        panel2.add(resetButton);
+        panel2.add(printPointsButton);
+        
+        pane.add(panel1);
+        pane.add(panel2);
+        
         applyMovementsButton
             .addActionListener(listener -> {
 
                 String text = solutionTextField.getText();
                 int[] moves = fromStringToInt(text.split(","));
                 Controller controller = new Controller();
-                residues = controller.parseInput(PROTEIN_CHAIN, moves);
+                String chain = chainTextField.getText();
+                residues = controller.parseInput(chain, moves);
                 grid = new Controller().generateGrid(residues);
 
                 energyLabel.setText("Energy value: " + ResidueUtils.getTopologyContacts(residues, grid).size());
@@ -163,36 +123,37 @@ public class ApplyFixedRelativeSolution {
                 chainLengthLabel.setText("Chain length: " + residues.size());
                 maxDistanceLabel.setText("Max Distance: " + ResidueUtils.getMaxPointsDistance(residues));
 
-                slots = (getBound(grid.getMatrix()) + 8);
+                slots = (getBound(grid.getMatrix()) + 3);
+                System.out.println(slots);
                 maxGridSize = calculateMaxGridSize(grid.getMatrix());
-                sizeFactor = MAX_SIZE_SCROLL_PANEL / slots;
-                if (sizeFactor < MIN_SIZE_FACTOR) {
-                    sizeFactor = MIN_SIZE_FACTOR;
-                }
+                
                 myCanvas.repaint();
             });
+        
         resetButton.addActionListener(listener -> {
 
             Runnable task = () -> {
                 residues = null;
-                // energyLabel.setText("Energy value: "
-                // + ResidueUtils.getTopologyContacts(residuesList, new
-                // Controller().generateGrid(residues))
-                // .size());
-                jScrollPane.repaint();
+                energyLabel.setText("Energy value: 0");
+                collisionsLabel.setText("Collisions: 0");
+                fitnessLabel.setText("Fitness: 0");
+                chainLengthLabel.setText("Chain length: 0");
+                maxDistanceLabel.setText("Max Distance: 0");
+                slots = 0;
+                myCanvas.repaint();
             };
             new Thread(task).start();
         });
 
         printPointsButton.addActionListener(listener -> {
-            for (int j = 0; j < residues.size(); j++) {
-                System.out.println("residues.add(new Residue(new Point(" + residues.get(j).getPoint().getX() + ", "
-                    + residues.get(j).getPoint().getY() + "), ResidueType.valueOf(String.valueOf(PROTEIN_CHAIN.charAt("
-                    + j + ")))));");
-            }
-
+        	if(residues != null) {
+	        	for (int j = 0; j < residues.size(); j++) {
+	                System.out.println("residues.add(new Residue(new Point(" + residues.get(j).getPoint().getX() + ", "
+	                    + residues.get(j).getPoint().getY() + "), ResidueType.valueOf(String.valueOf(PROTEIN_CHAIN.charAt("
+	                    + j + ")))));");
+	            }
+        	}
         });
-
     }
 
     private static int[] fromStringToInt(String[] moves) {
@@ -210,7 +171,7 @@ public class ApplyFixedRelativeSolution {
     }
 
     private static int getBound(int[][] matrix) {
-
+    	
         int maxX = -1;
         int maxY = -1;
         int minX = matrix.length;
@@ -256,6 +217,7 @@ public class ApplyFixedRelativeSolution {
 
         // Size and display the window.
         frame.setSize(SCREEN_SIZE, SCREEN_SIZE);
+        frame.pack();
         frame.setVisible(true);
     }
 
@@ -274,6 +236,9 @@ public class ApplyFixedRelativeSolution {
 
     static class MyCanvas extends JPanel implements MouseListener {
 
+    	private int WIDTH;
+    	private int HEIGHT;
+    	
         public MyCanvas() {
 
             addMouseListener(this);
@@ -281,18 +246,28 @@ public class ApplyFixedRelativeSolution {
 
         @Override
         public void paint(Graphics g) {
-
+        	        	
+        	WIDTH = HEIGHT = (maxGridSize > 500) ? maxGridSize : 480;
+        	
+        	System.out.println("WIDTH: "+WIDTH+" | "+getWidth());
+        	
+        	Graphics2D g2d = (Graphics2D) g.create(0, 0, WIDTH, HEIGHT);
+        	
+        	g2d.setColor(Color.white);
+        	g2d.fillRect(0, 0, WIDTH, HEIGHT);
+        	g2d.setColor(Color.LIGHT_GRAY);
+        	
             for (int i = 0; i < slots; i++) {
                 for (int j = 0; j < slots; j++) {
-                    int rectX = sizeFactor * (i);
-                    int rectY = sizeFactor * (j);
-                    g.drawRect(rectX, rectY, sizeFactor, sizeFactor);
-
+                    int rectX = sizeFactor * i;
+                    int rectY = sizeFactor * j;
+                    g2d.drawRect(rectX, rectY, sizeFactor, sizeFactor);
                 }
             }
+            
             Residue next = null;
             if (residues != null) {
-
+            	
                 for (int i = 0; i < residues.size(); i++) {
                     if (i != residues.size() - 1) {
                         next = residues.get(i + 1);
@@ -300,24 +275,23 @@ public class ApplyFixedRelativeSolution {
                     int residueX = sizeFactor * (residues.get(i).getPoint().getX()) + sizeFactor / 4;
                     int residueY = sizeFactor * (residues.get(i).getPoint().getY()) + sizeFactor / 4;
                     if (residues.get(i).getResidueType().equals(ResidueType.H)) {
-                        g.setColor(Color.BLACK);
-                        g.fillArc(residueX, residueY, sizeFactor / 2, sizeFactor / 2, 0, 360);
+                        g2d.setColor(Color.BLACK);
+                        g2d.fillRect(residueX, residueY, sizeFactor / 2, sizeFactor / 2);//desenha quadrado
+                        //g2d.fillArc(residueX, residueY, sizeFactor / 2, sizeFactor / 2, 0, 360);//desenha bolinha
                     } else {
-                        g.setColor(Color.LIGHT_GRAY);
-                        g.fillArc(residueX, residueY, sizeFactor / 2, sizeFactor / 2, 0, 360);
+                        g2d.setColor(Color.LIGHT_GRAY);
+                        g2d.fillArc(residueX, residueY, sizeFactor / 2, sizeFactor / 2, 0, 360);
                     }
-                    g.setColor(Color.BLACK);
+                    g2d.setColor(Color.BLACK);
 
                     if (next != null) {
                         int nextX = sizeFactor * (next.getPoint().getX()) + sizeFactor / 4;
                         int nextY = sizeFactor * (next.getPoint().getY()) + sizeFactor / 4;
-                        g.drawLine(residueX + sizeFactor / 4, residueY + sizeFactor / 4, nextX + sizeFactor / 4, nextY
+                        g2d.drawLine(residueX + sizeFactor / 4, residueY + sizeFactor / 4, nextX + sizeFactor / 4, nextY
                             + sizeFactor / 4);
                     }
-
-                }
+                }                
             }
-
         }
 
         private int getMinX(List<Residue> residues) {
